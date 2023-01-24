f8a2313a2b02a2a31630115c7520a2a479483bb8
Steven <94455238+bymyselfstudio@users.noreply.github.com>
2023-01-24 17:17:46 +0100

    Update manual_upper_lower.py
    Reduced amount of line breaks

--- manual_upper_lower.py
+++ manual_upper_lower.py
@@ -4,14 +4,12 @@ def getLetters(words: str) -> list[str]:
         letters.append(letter)
     return letters
 
-
 def getAsciiCode(letters: list[str]) -> list[int]:
     asciiCodes = []
     for i in range(len(letters)):
         correspondingAsciiCode = ord(letters[i])
         asciiCodes.append(correspondingAsciiCode)
     return asciiCodes
-
 
 def swapToLower(asciiCode: list[int]):
     for i in range(len(asciiCode)):
@@ -20,14 +18,12 @@ def swapToLower(asciiCode: list[int]):
         else:
             print(chr(asciiCode[i]), end="")
 
-
 def swapToUpper(asciiCode: list[int]):
     for i in range(len(asciiCode)):
         if asciiCode[i] >= 97 and asciiCode[i] <= 122:
             print(chr(asciiCode[i] - 32), end="")
         else:
             print(chr(asciiCode[i]), end="")
-
 
 def main():
     words = "HeLlo WoRLd"
@@ -36,7 +32,6 @@ def main():
     swapToUpper(asciiCode)
     print()
     swapToLower(asciiCode)
-
 
 if __name__ == "__main__":
     main()
