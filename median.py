4f8e41acaa1bd2d0ce380269d571fc13d535e8fe
Steven <94455238+bymyselfstudio@users.noreply.github.com>
2023-01-24 17:19:26 +0100

    Update median.py
    Reduced amount of line breaks

--- median.py
+++ median.py
@@ -2,13 +2,11 @@ def getNumbersAmount() -> int:
     amount = input("How many numbers will be in the list? ")
     return int(amount)
 
-
 def getNumbers(amount: int) -> list[float]:
     numbers = []
     for i in range(amount):
         numbers.append(float(input(f"Input {i+1}. number: ")))
     return numbers
-
 
 def getNumbersSum(numbers: list[float]) -> float:
     sum = 0
@@ -16,11 +14,9 @@ def getNumbersSum(numbers: list[float]) -> float:
         sum += numbers[i]
     return sum
 
-
 def getMedian(sum: float, numbers: list[float]) -> float:
     divisor = len(numbers)
     return sum / divisor
-
 
 def main():
     amount = getNumbersAmount()
@@ -28,7 +24,6 @@ def main():
     sum = getNumbersSum(numbers)
     average = getMedian(sum, numbers)
     print(f"Average value of {numbers} is {average:.3f}")
-
 
 if __name__ == "__main__":
     main()
